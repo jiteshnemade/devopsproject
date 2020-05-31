9f7f8d6649190c65d90077513d14f99f7a6f4890
jiteshnemade <jiteshnemade@gmail.com>
2020-05-31 13:55:38 +0530

    properties updated

--- src/it/java/org/queryIT.java
+++ src/it/java/org/queryIT.java
@@ -17,64 +17,31 @@ import java.util.concurrent.TimeUnit;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class queryIT {
     private static WebDriver driver;
-    //String URL = "http://localhost:8081/";
     String URL="http://localhost:8081/queryForum_war/";
 
     @BeforeClass
     public static void setDriver() {
-
         System.setProperty("webdriver.gecko.driver", "geckodriver");
-
         DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
-
-
+        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR,
+                UnexpectedAlertBehaviour.IGNORE);
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.addArguments("--headless");
-
         driver = new FirefoxDriver(firefoxOptions);
-
     }
-    /*@Test
-    public void verifyDiv() {
-
-        String ans="";
-        WebDriverWait wait= new WebDriverWait(driver, 1);
-        Alert alert;
-        try {
-            driver.navigate().to(URL); // String getTitle = driver.getTitle(); //Scanner
-            driver.findElement(By.id("op1")).sendKeys("10");
-            driver.findElement(By.id("op2")).sendKeys("5");
-            driver.findElement(By.id("divbtn")).click();
-
-            wait.until(ExpectedConditions.alertIsPresent());
-
-            alert= driver.switchTo().alert();
-            ans =alert.getText().trim();
-            alert.accept();
-        }
-        catch (Exception e) {
-            System.out.println("In exception");
-        }
-        Assert.assertEquals(ans, "Answer = 2.0");
-    }*/
 
     @Test
     public void it1VerifyHomePageTittle() {
-        // driver.navigate().to(URL);
         driver.get(URL);
         WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("loginbtn"))));
-
         String getTitle = driver.getTitle();
         System.out.println(getTitle);
-        // driver.quit();
         Assert.assertEquals( "IIITB Query Forum",getTitle);
     }
 
     @Test
     public void it3Login() {
-        //Verify Home page
         driver.get(URL);
         WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("signuplink"))));
@@ -100,15 +67,10 @@ public class queryIT {
         Assert.assertTrue(currPage.contains("sign_up.html"));
     }
 
-    @Test
+    /*@Test
     public void it4Logout(){
         driver.get(URL);
         WebDriverWait wait = new WebDriverWait(driver, 20);
-      /*  wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("signuplink"))));
-
-        driver.findElement(By.id("username")).sendKeys("testuser");
-        driver.findElement(By.id("password")).sendKeys("test123");
-        driver.findElement(By.id("loginbtn")).click();*/
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("logoutlink"))));
 
@@ -120,7 +82,7 @@ public class queryIT {
 
 
     }
-
+*/
     @AfterClass
     public static void delDriver() {
         driver.quit();
